59bb3a2f15a1857f8cc7717ea5ef7e8bc046fc9c
rootellen <ruthellennds@gmail.com>
2021-08-18 23:53:55 -0300

    cadastro formulario completo

--- app/src/main/java/com/example/cadastro/MainActivity.java
+++ app/src/main/java/com/example/cadastro/MainActivity.java
@@ -3,12 +3,78 @@ package com.example.cadastro;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.Spinner;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.cadastro.dominio.Formulario;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText nomeEditText;
+    private EditText telefoneEditText;
+    private EditText emailEditText;
+    private CheckBox listaEmailsCheckBox;
+    private RadioButton masculinoRadioButton;
+    private EditText cidadeEditText;
+    private Spinner estadoSpinner;
+    private Button limparButton;
+    private Button salvarButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //atribuir ids
+        nomeEditText = findViewById(R.id.nomeEt);
+        telefoneEditText = findViewById(R.id.telefoneEt);
+        emailEditText = findViewById(R.id.emailEt);
+        listaEmailsCheckBox = findViewById(R.id.listaEmailCheckBox);
+        masculinoRadioButton = findViewById(R.id.masculinoRb);
+        cidadeEditText = findViewById(R.id.cidadeEt);
+        estadoSpinner = findViewById(R.id.estadosSp);
+        limparButton = findViewById(R.id.limparBt);
+        salvarButton = findViewById(R.id.salvarBt);
+        salvarButton.setOnClickListener(this);
+        limparButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.salvarBt) {
+            Formulario formulario = new Formulario();
+            formulario.setNomeCompleto(nomeEditText.getText().toString());
+            formulario.setTelefone(telefoneEditText.getText().toString());
+            formulario.setEmail(emailEditText.getText().toString());
+            if (listaEmailsCheckBox.isChecked()) {
+                formulario.setListaEmails(true);
+            } else {
+                formulario.setListaEmails(false);
+            }
+            if (masculinoRadioButton.isChecked()) {
+                formulario.setSexo("Masculino");
+            } else {
+                formulario.setSexo("Feminino");
+            }
+            formulario.setCidade(cidadeEditText.getText().toString());
+            //String UF = estadoSpinner.getSelectedView().toString();
+            formulario.setUF(estadoSpinner.getSelectedItem().toString());
+            Toast.makeText(this, formulario.toString(), Toast.LENGTH_LONG).show();
+        }
+
+        if (view.getId() == R.id.limparBt){
+            nomeEditText.setText("");
+            telefoneEditText.setText("");
+            emailEditText.setText("");
+            listaEmailsCheckBox.setChecked(false);
+            masculinoRadioButton.setChecked(true);
+            cidadeEditText.setText("");
+            estadoSpinner.setSelection(0);
+        }
     }
 }
--- app/src/main/java/com/example/cadastro/dominio/Formulario.java
+++ app/src/main/java/com/example/cadastro/dominio/Formulario.java
@@ -9,13 +9,73 @@ public class Formulario {
     private String cidade;
     private String UF;
 
-    public Formulario(String nomeCompleto, String telefone, String email, boolean listaEmails, String sexo, String cidade, String UF) {
+    public Formulario(){}
+
+    public String getNomeCompleto() {
+        return nomeCompleto;
+    }
+
+    public void setNomeCompleto(String nomeCompleto) {
         this.nomeCompleto = nomeCompleto;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
         this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
         this.email = email;
+    }
+
+    public boolean isListaEmails() {
+        return listaEmails;
+    }
+
+    public void setListaEmails(boolean listaEmails) {
         this.listaEmails = listaEmails;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
         this.sexo = sexo;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
         this.cidade = cidade;
+    }
+
+    public String getUF() {
+        return UF;
+    }
+
+    public void setUF(String UF) {
         this.UF = UF;
+    }
+
+    @Override
+    public String toString() {
+        return "Formulario:\n" +
+                "Nome Completo='" + nomeCompleto + "'\n" +
+                "Telefone='" + telefone + "'\n" +
+                "E-mail='" + email + "'\n" +
+                "ListaEmails=" + listaEmails + "\n" +
+                "Sexo='" + sexo + "'\n" +
+                "Cidade='" + cidade + "'\n" +
+                "UF='" + UF + '\'';
     }
 }
